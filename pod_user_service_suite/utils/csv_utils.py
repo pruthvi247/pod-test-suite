7f93af0477d4479b64846107a1159c02b3cc6ac3
Pruthvi Kumar <pruthvi.kumar@mac-pruthvikumar.local>
2019-12-01 20:47:59 +0530

    added html functionality

--- pod_user_service_suite/utils/csv_utils.py
+++ pod_user_service_suite/utils/csv_utils.py
@@ -2,6 +2,8 @@
 
 import pandas as pd
 import logging
+from json2html import *
+import json
 
 from pod_user_service_suite.utils.json_utils import load_properties_from_json
 
@@ -18,4 +20,17 @@ def write_to_csv(filepath,dict):
     # pd.DataFrame(dict).to_csv(filepath,index=False)
     pdseries = pd.Series(dict).to_frame()
     pd.DataFrame(pdseries).to_csv(filepath,header=['output'])
+    return pd.read_csv(filepath)
+def write_to_html(filepath):
+    logger.debug("writing status output to html : {}".format(filepath))
+    df = pd.read_csv(filepath)
+    newdict = {}
+    for items in df.itertuples():
+        newdict[items[1]] = items[2]
+    r = json.dumps(newdict)
+    outhtml = json2html.convert(json=r)
+    filename = filepath.replace('.csv', '.html')
+    f = open(filename, 'w')
+    f.write(outhtml)
+    f.close()
     return pd.read_csv(filepath)
