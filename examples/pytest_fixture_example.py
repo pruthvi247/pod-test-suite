674ca9740c95c4e8d6b3be7fa87f0981f0c5b2b2
pruthvikumar <pruthvikumar@pruthvis-MacBook-Pro.local>
2020-09-08 16:11:44 +0530

    added pytest fixture example

--- examples/pytest_fixture_example.py
+++ examples/pytest_fixture_example.py
@@ -3,8 +3,9 @@ import pytest
 
 def get_data():
     # Retrieve values from CSV
-    return [['a','b','c'], 'bb', 'cc']
+    return [['a','b','c'], {"key":'bb'}, 'cc']
 
 @pytest.mark.parametrize("value", get_data())
 def test_sample(value):
     print(f'input params given from function : {value}')
+    print(f'input params given from function : {type(value)}')
